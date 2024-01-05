b04270d67abed043f9a9cd5c75423842193a0f1b
nGhart <107068059+nGhart@users.noreply.github.com>
2024-01-05 19:06:35 +0000

    Update Landing.jsx container

--- mes-client/src/page/Landing.jsx
+++ mes-client/src/page/Landing.jsx
@@ -2,18 +2,17 @@ import {Link } from 'react-router-dom'
 const Landing=()=>{
     return (
       
-        <div className="card">
-  <h5 className="card-header">Welcome to the orders app</h5>
-  <div className="card-body">
-    <h5 className="card-title">Log in with your credentials</h5>
-   
-    <div className="landingButtons">
-        {/* <Link to='register'>
-    <a href="#" className="btn btn-primary">Register</a></Link> */}
-    <Link to='login'> <a href="#" className="btn btn-primary">Login</a></Link>
-    </div>
-  </div>
-</div>
+      <div className='landingContainer'>
+
+       <h2> Welcome to the Cookiez order List beads N more</h2>
+        <h5 className="">Log in with your credentials</h5>
+       
+        <div className="landingButtons">
+          
+        <Link to='login'> <a href="#" className="btn btn-primary">Login</a></Link>
+        </div>
+      </div>
+    
     )
 }
 export default Landing
