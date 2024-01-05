68c8cc126db356da41f88e4c15b482cc17e655ce
nGhart <107068059+nGhart@users.noreply.github.com>
2024-01-05 20:34:26 +0000

    Update messageController.js message status commented out

--- mes-server/controller/messageController.js
+++ mes-server/controller/messageController.js
@@ -37,6 +37,43 @@ return axios.request(config)
     });
 }
 
+
+// function sendWhatsAppMessage (req, res) {
+//   const data = JSON.stringify({
+//     "messaging_product": "whatsapp",
+//     "to": process.env.RECIPIENT_WAID,
+//     "type": "template",
+//     "template": {
+//       //"name": "hello_world",
+//       "name": "order_received",
+//       "language": {
+//         "code": "en_US"
+//       }
+//     }
+//   });
+
+//   const config = {
+//     method: 'post',
+//     maxBodyLength: Infinity,
+//     url: `https://graph.facebook.com/${process.env.VERSION}/${process.env.PHONE_NUMBER_ID}/messages`,
+//     headers: {
+//       'Content-Type': 'application/json',
+//       'Authorization': `Bearer ${process.env.ACCESS_TOKEN}`
+//     },
+//     data: data
+//   };
+
+// axios.request(config)
+//     .then(response => {
+//       console.log("Message delivered")
+//            //res.status(response.status).json(response.data);
+//     })
+//     .catch(error => {
+//       // console.error('Error sending WhatsApp message:', error.message);
+//       res.status(500).json({ error: 'Internal Server Error' });
+//     });
+// }
+
 module.exports = {
   sendWhatsAppMessage,
 };
